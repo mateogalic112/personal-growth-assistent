633658ce67e92b6fb67c50d91474465103bd7456
Mateo Galić <mateo@agilo.co>
2021-03-26 11:51:00 +0100

    Making search field right dimensions.

--- frontend/src/widgets/Search/style.js
+++ frontend/src/widgets/Search/style.js
@@ -2,13 +2,18 @@ import styled from 'styled-components';
 
 export const SearchBar = styled.div`
 	height: 2rem;
-	width: 16rem;
+	width: 100%;
+	max-width: 16rem;
 	position: relative;
 	font-size: 0.7rem;
 	transition: box-shadow 0.2s, opacity 0.2s;
 	border-radius: 5rem;
 	opacity: 0.7;
 	margin-top: .5rem;
+
+	@media (min-width: 768px) {
+		width: 16rem;
+	}
 
 	&:hover {
 		box-shadow: 0px 0px 5px 3px rgba(68, 68, 68, 0.2);
@@ -17,12 +22,17 @@ export const SearchBar = styled.div`
 
 	input {
 		height: 2rem;
-		width: 16rem;
+		max-width: 16rem;
+		width: 100%;
 		padding: 0 2rem;
 		border-radius: 5rem;
 		border: none;
 		outline: none;
 		color: #14121f;
+
+		@media (min-width: 768px) {
+			width: 16rem;
+		}
 
 		&::placeholder {
 			font-size: 0.7rem;
