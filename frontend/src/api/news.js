526433ad8a711768cd7aee528d5a23a6d72efa00
Mateo Galic <mateogalic112@gmail.com>
2021-05-08 13:22:45 +0200

    News api

--- frontend/src/api/news.js
+++ frontend/src/api/news.js
@@ -1,7 +1,7 @@
 const BASE = 'https://newsapi.org';
 
 export const newsQuery = async (param) => {
-	const queryNewsUrl = `${BASE}/v2/everything?q=${param}&apiKey=${process.env.REACT_APP_NEWS_API_KEY}&pageSize=20`;
+	const queryNewsUrl = `${BASE}/v2/everything?q=${param}&apiKey=79a2b4c9a092459aaca2754438e92912&pageSize=20`;
 
 	const response = await fetch(queryNewsUrl);
 
