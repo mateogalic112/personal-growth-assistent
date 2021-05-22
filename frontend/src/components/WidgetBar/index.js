9757442b69ebe8851ff396f2a19bb93b54c83044
Mateo Galic <mateogalic112@gmail.com>
2021-05-22 23:22:43 +0200

    Deleting notifications from widget bar.

--- frontend/src/components/WidgetBar/index.js
+++ frontend/src/components/WidgetBar/index.js
@@ -1,19 +1,13 @@
 import Search from '../../widgets/Search';
 import Logout from '../../widgets/Logout';
-import Notifier from '../../widgets/Notifier';
 
 import { WidgetContainer, WidgetRow } from './style';
-
-import { notifierData } from '../../widgets/Notifier/data';
 
 const WidgetBar = () => {
 	return (
 		<WidgetContainer>
 			<Search />
 			<WidgetRow>
-				{notifierData.map((item, index) => (
-					<Notifier key={index}>{item.icon}</Notifier>
-				))}
 				<Logout />
 			</WidgetRow>
 		</WidgetContainer>
